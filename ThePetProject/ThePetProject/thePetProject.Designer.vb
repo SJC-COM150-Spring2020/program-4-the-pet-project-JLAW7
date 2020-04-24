919a85f8c4893751d4c146616208ff372d9d0832
JLAW7 <60242914+JLAW7@users.noreply.github.com>
2020-04-23 23:54:51 -0400

    Created a program to vote for favorite pet
    Allows multiple users to vote for their favorite pet.

---
+++ ThePetProject/ThePetProject/thePetProject.Designer.vb
@@ -0,0 +1,247 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Pet
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Pet))
+        Me.btnVote = New System.Windows.Forms.Button()
+        Me.lstOut = New System.Windows.Forms.ListBox()
+        Me.lblTitle = New System.Windows.Forms.Label()
+        Me.picDog = New System.Windows.Forms.PictureBox()
+        Me.picCat = New System.Windows.Forms.PictureBox()
+        Me.picFish = New System.Windows.Forms.PictureBox()
+        Me.picBird = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        CType(Me.picDog, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picCat, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picFish, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picBird, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnVote
+        '
+        Me.btnVote.Font = New System.Drawing.Font("Segoe UI", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnVote.Location = New System.Drawing.Point(148, 487)
+        Me.btnVote.Name = "btnVote"
+        Me.btnVote.Size = New System.Drawing.Size(175, 34)
+        Me.btnVote.TabIndex = 0
+        Me.btnVote.Text = "VOTE"
+        Me.btnVote.UseVisualStyleBackColor = True
+        '
+        'lstOut
+        '
+        Me.lstOut.Font = New System.Drawing.Font("Segoe UI Semibold", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lstOut.FormattingEnabled = True
+        Me.lstOut.ItemHeight = 21
+        Me.lstOut.Location = New System.Drawing.Point(12, 534)
+        Me.lstOut.Name = "lstOut"
+        Me.lstOut.Size = New System.Drawing.Size(436, 67)
+        Me.lstOut.TabIndex = 1
+        '
+        'lblTitle
+        '
+        Me.lblTitle.AutoSize = True
+        Me.lblTitle.Font = New System.Drawing.Font("Segoe UI", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTitle.Location = New System.Drawing.Point(125, 30)
+        Me.lblTitle.Name = "lblTitle"
+        Me.lblTitle.Size = New System.Drawing.Size(219, 37)
+        Me.lblTitle.TabIndex = 2
+        Me.lblTitle.Text = "The Pet Contest"
+        '
+        'picDog
+        '
+        Me.picDog.Image = CType(resources.GetObject("picDog.Image"), System.Drawing.Image)
+        Me.picDog.Location = New System.Drawing.Point(464, 12)
+        Me.picDog.Name = "picDog"
+        Me.picDog.Size = New System.Drawing.Size(357, 645)
+        Me.picDog.TabIndex = 3
+        Me.picDog.TabStop = False
+        Me.picDog.Visible = False
+        '
+        'picCat
+        '
+        Me.picCat.Image = CType(resources.GetObject("picCat.Image"), System.Drawing.Image)
+        Me.picCat.Location = New System.Drawing.Point(464, 137)
+        Me.picCat.Name = "picCat"
+        Me.picCat.Size = New System.Drawing.Size(357, 334)
+        Me.picCat.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage
+        Me.picCat.TabIndex = 4
+        Me.picCat.TabStop = False
+        Me.picCat.Visible = False
+        '
+        'picFish
+        '
+        Me.picFish.Image = CType(resources.GetObject("picFish.Image"), System.Drawing.Image)
+        Me.picFish.Location = New System.Drawing.Point(464, 179)
+        Me.picFish.Name = "picFish"
+        Me.picFish.Size = New System.Drawing.Size(357, 272)
+        Me.picFish.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picFish.TabIndex = 5
+        Me.picFish.TabStop = False
+        Me.picFish.Visible = False
+        '
+        'picBird
+        '
+        Me.picBird.Image = CType(resources.GetObject("picBird.Image"), System.Drawing.Image)
+        Me.picBird.Location = New System.Drawing.Point(484, 159)
+        Me.picBird.Name = "picBird"
+        Me.picBird.Size = New System.Drawing.Size(321, 292)
+        Me.picBird.TabIndex = 6
+        Me.picBird.TabStop = False
+        Me.picBird.Visible = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(61, 92)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(341, 30)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "WELCOME TO THE PET CONTEST!"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Segoe UI Semibold", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(63, 133)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(339, 210)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = resources.GetString("Label2.Text")
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Segoe UI", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(86, 367)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(106, 32)
+        Me.Label3.TabIndex = 9
+        Me.Label3.Text = "A : DOG"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Segoe UI", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(286, 365)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(95, 32)
+        Me.Label4.TabIndex = 10
+        Me.Label4.Text = "B : CAT"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Segoe UI", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(85, 439)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(102, 32)
+        Me.Label5.TabIndex = 11
+        Me.Label5.Text = "C : FISH"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Segoe UI", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(285, 439)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(111, 32)
+        Me.Label6.TabIndex = 12
+        Me.Label6.Text = "D : BIRD"
+        '
+        'btnClear
+        '
+        Me.btnClear.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClear.Location = New System.Drawing.Point(50, 617)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(128, 40)
+        Me.btnClear.TabIndex = 13
+        Me.btnClear.Text = "CLEAR"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExit.Location = New System.Drawing.Point(291, 617)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(128, 40)
+        Me.btnExit.TabIndex = 14
+        Me.btnExit.Text = "EXIT"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Pet
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.DeepSkyBlue
+        Me.ClientSize = New System.Drawing.Size(833, 659)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.picCat)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.picBird)
+        Me.Controls.Add(Me.picFish)
+        Me.Controls.Add(Me.lblTitle)
+        Me.Controls.Add(Me.lstOut)
+        Me.Controls.Add(Me.btnVote)
+        Me.Controls.Add(Me.picDog)
+        Me.Name = "Pet"
+        Me.Text = "The Pet Project"
+        CType(Me.picDog, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picCat, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picFish, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picBird, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnVote As Button
+    Friend WithEvents lstOut As ListBox
+    Friend WithEvents lblTitle As Label
+    Friend WithEvents picDog As PictureBox
+    Friend WithEvents picCat As PictureBox
+    Friend WithEvents picFish As PictureBox
+    Friend WithEvents picBird As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnExit As Button
+End Class
---
+++ ThePetProject/ThePetProject/thePetProject.vb
@@ -0,0 +1,117 @@
+﻿Public Class Pet
+
+    'Define and initialize program
+
+    Private Sub btnVote_Click(sender As Object, e As EventArgs) Handles btnVote.Click
+
+        'Dim and initialize I as String, and A, B, C, and D as Integers 
+
+        Dim I As String
+
+        Dim A, B, C, D As Integer
+
+        'Letting Inputbox equal I and inputting a Do until loop with If Then, If Else statements including message boxes. Then ouputing total votes to listbox
+
+        Do
+            I = InputBox("Type the Letter You wish to vote for. Then click ok.")
+            If (I = "A") Then
+                A = A + 1
+            ElseIf (I = "B") Then
+                B = B + 1
+            ElseIf (I = "C") Then
+                C = C + 1
+            ElseIf (I = "D") Then
+                D = D + 1
+            ElseIf (I = "E") Then
+                MessageBox.Show("Thank you for your votes! ")
+            ElseIf (I = "") Then
+                MessageBox.Show("Please input the letter associated with your animal, then click ""ok"". ")
+            Else
+                MessageBox.Show("I'm sorry, that is not a valid choice. Please try again.")
+            End If
+        Loop Until (I = "E")
+        lstOut.Items.Add("Dog Votes " & A)
+        lstOut.Items.Add("Cat Votes " & B)
+        lstOut.Items.Add("Fish Votes " & C)
+        lstOut.Items.Add("Bird Votes " & D)
+
+        'After loop, placing additional If Then, elseIf statements comparing totals and listing winner into listbox revealing winner with a picture
+        'In addition, inlcuding scenarios where ties occur and asking user to correct issue with additional voting
+
+
+        If (A > B And A > C And A > D) Then
+            lstOut.Items.Add("The winner is Dog!")
+            picDog.Visible = True
+            picCat.Visible = False
+            picFish.Visible = False
+            picBird.Visible = False
+        ElseIf (B > A And B > C And B > D) Then
+            lstOut.Items.Add("The winner is Cat!")
+            picCat.Visible = True
+            picDog.Visible = False
+            picFish.Visible = False
+            picBird.Visible = False
+        ElseIf (C > A And C > B And C > D) Then
+            lstOut.Items.Add("The winner is Fish!")
+            picFish.Visible = True
+            picCat.Visible = False
+            picDog.Visible = False
+            picBird.Visible = False
+        ElseIf (D > A And D > B And D > C) Then
+            lstOut.Items.Add("The winner is Bird!")
+            picBird.Visible = True
+            picCat.Visible = False
+            picFish.Visible = False
+            picDog.Visible = False
+        ElseIf (A = B And A > C And A > D) Then
+            lstOut.Items.Add("We have a tie between Dog and Cat!")
+            lstOut.Items.Add("Next User, please place your vote for Dog or Cat to break the tie.")
+        ElseIf (A = C And A > B And A > D) Then
+            lstOut.Items.Add("We have a tie between Dog and Fish!")
+            lstOut.Items.Add("Next User, please place your vote for Dog or Fish to break the tie.")
+        ElseIf (A = D And A > B And A > C) Then
+            lstOut.Items.Add("We have a tie between Dog and Bird!")
+            lstOut.Items.Add("Next User, please place your vote for Dog or Bird to break the tie.")
+        ElseIf (B = C And B > A And B > D) Then
+            lstOut.Items.Add("We have a tie between Cat and Fish!")
+            lstOut.Items.Add("Next User, please place your vote for Cat or Fish to break the tie.")
+        ElseIf (B = D And B > A And B > C) Then
+            lstOut.Items.Add("We have a tie between Cat and Bird!")
+            lstOut.Items.Add("Next User, please place your vote for Cat or Bird to break the tie.")
+        ElseIf (C = D And C > A And C > D) Then
+            lstOut.Items.Add("We have a tie between Fish and Bird!")
+            lstOut.Items.Add("Next User, please place your vote for Fish or Bird to break the tie.")
+        ElseIf (A = B And A = C And A > D) Then
+            lstOut.Items.Add("We have a 3-Way tie between Dog, Cat, and Fish!")
+            lstOut.Items.Add("Next User, please place your vote for Dog, Cat, or Fish to break the tie.")
+        ElseIf (A = B And A = D And A > C) Then
+            lstOut.Items.Add("We have a 3-way tie between Dog, Cat, and Bird!")
+            lstOut.Items.Add("Next User, please place your vote for Dog, Cat, or Bird to break the tie.")
+        ElseIf (A = C And A = D And A > B) Then
+            lstOut.Items.Add("We have a 3-way tie between Dog, Fish, and Bird!")
+            lstOut.Items.Add("Next User, please place your vote for Dog, Fish, or Bird to break the tie.")
+        ElseIf (B = C And B = D And B > A) Then
+            lstOut.Items.Add("We have a 3-way tie between Cat, Fish, and Bird!")
+            lstOut.Items.Add("Next User, please place your vote for Cat, Fish, or Bird to break the tie.")
+        ElseIf (A = B And A = C And A = D) Then
+            lstOut.Items.Add("We have a 4-way tie between Dog, Cat, Fish, and Bird!")
+            lstOut.Items.Add("Next User, please place your vote for Dog, Cat, Fish, or Bird to break the tie.")
+        End If
+
+    End Sub
+
+    'Initialize clear button for list box
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+
+        lstOut.Items.Clear()
+
+    End Sub
+
+    'initailize exit button for program
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Beep()
+        Application.Exit()
+    End Sub
+End Class
